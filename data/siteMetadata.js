2418243eeb535707fa1c78416fb23e4f31aeca16
Matthew Dorey <mattischrome@gmail.com>
2022-04-05 21:41:30 +0100

    Fix broken SiteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -17,20 +17,20 @@ const siteMetadata = {
   youtube: 'https://youtube.com',
   linkedin: 'https://www.linkedin.com',
   locale: 'en-GB',
-  // analytics: {
-  //   // If you want to use an analytics provider you have to add it to the
-  //   // content security policy in the `next.config.js` file.
-  //   // supports plausible, simpleAnalytics, umami or googleAnalytics
-  //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-  //   simpleAnalytics: false, // true or false
-  //   umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-  //   googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
-  // },
-  // newsletter: {
-  //   // supports mailchimp, buttondown, convertkit, klaviyo, revue
-  //   // Please add your .env file and modify it according to your selection
-  //   provider: 'buttondown',
-  // },
+  analytics: {
+    // If you want to use an analytics provider you have to add it to the
+    // content security policy in the `next.config.js` file.
+    // supports plausible, simpleAnalytics, umami or googleAnalytics
+    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    simpleAnalytics: false, // true or false
+    umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
+    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
+  },
+  newsletter: {
+    // supports mailchimp, buttondown, convertkit, klaviyo, revue
+    // Please add your .env file and modify it according to your selection
+    provider: 'buttondown',
+  },
   comment: {
     // If you want to use a commenting system other than giscus you have to add it to the
     // content security policy in the `next.config.js` file.
@@ -62,22 +62,22 @@ const siteMetadata = {
       // example: https://giscus.app/themes/custom_example.css
       themeURL: '',
     },
-    // utterancesConfig: {
-    //   // Visit the link below, and follow the steps in the 'configuration' section
-    //   // https://utteranc.es/
-    //   repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
-    //   issueTerm: '', // supported options: pathname, url, title
-    //   label: '', // label (optional): Comment 💬
-    //   // theme example: github-light, github-dark, preferred-color-scheme
-    //   // github-dark-orange, icy-dark, dark-blue, photon-dark, boxy-light
-    //   theme: '',
-    //   // theme when dark mode
-    //   darkTheme: '',
-    // },
-    // disqusConfig: {
-    //   // https://help.disqus.com/en/articles/1717111-what-s-a-shortname
-    //   shortname: process.env.NEXT_PUBLIC_DISQUS_SHORTNAME,
-    // },
+    utterancesConfig: {
+      // Visit the link below, and follow the steps in the 'configuration' section
+      // https://utteranc.es/
+      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
+      issueTerm: '', // supported options: pathname, url, title
+      label: '', // label (optional): Comment 💬
+      // theme example: github-light, github-dark, preferred-color-scheme
+      // github-dark-orange, icy-dark, dark-blue, photon-dark, boxy-light
+      theme: '',
+      // theme when dark mode
+      darkTheme: '',
+    },
+    disqusConfig: {
+      // https://help.disqus.com/en/articles/1717111-what-s-a-shortname
+      shortname: process.env.NEXT_PUBLIC_DISQUS_SHORTNAME,
+    },
   },
 }
 
